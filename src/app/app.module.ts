0a0f3d69e4ffd043afaff095d9122a13fddd1e57
abhi-ye <y.abhi1997@gmail.com>
2022-02-08 18:59:39 +0530

    randomMeal Api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common'
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { mealsHomeComponent } from './mealsHome/mealsHome';
 import { mealsCardComponent } from './mealsCard/mealsCard';
+import { mealsDbService } from './services/mealsDb.service';
 
 
 
 @NgModule({
   imports: [
     BrowserModule,
+    CommonModule,
     ReactiveFormsModule,
     HttpClientModule,
     RouterModule.forRoot([
@@ -26,15 +29,10 @@ import { mealsCardComponent } from './mealsCard/mealsCard';
     mealsHomeComponent,
     mealsCardComponent
   ],
+  providers: [mealsDbService],
   bootstrap: [
     AppComponent
   ]
 })
 export class AppModule { }
 
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
--- src/app/services/mealsDb.service.ts
+++ src/app/services/mealsDb.service.ts
@@ -3,10 +3,10 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class mealsDbService {
-  private url = 'www.themealdb.com/api/json/v1/1'
+  private url = 'https://www.themealdb.com/api/json/v1/1'
   constructor(private http: HttpClient) { }
 
   getRandomMeal(){
-    return this.http.get(this.url + 'random.php');
+    return this.http.get(this.url + '/random.php');
   }
 }
